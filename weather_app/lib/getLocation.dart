66d7b205b9fd4fcc3b2a87b02f37324229629d2a
Aakrish Lama <al226na@student.lnu.se>
2025-07-22 13:54:55 +0200

    formating

---
+++ weather_app/lib/getLocation.dart
@@ -0,0 +1,76 @@
+import 'package:flutter/material.dart';
+import 'package:geolocator/geolocator.dart';
+import 'package:permission_handler/permission_handler.dart';
+
+class MyLocation extends StatefulWidget {
+  const MyLocation({super.key});
+
+  @override
+  State<MyLocation> createState() => _MyLocationState();
+}
+
+class _MyLocationState extends State<MyLocation> {
+  String? locationMessage;
+
+  Future<void> getCurrentLocation() async {
+    print("getCurrentLocation method started");
+    try {
+      // Request location permission
+      PermissionStatus permissionStatus = await Permission.location.request();
+
+      if (permissionStatus.isGranted) {
+        // Check if location services are enabled
+        bool isLocationEnabled = await Geolocator.isLocationServiceEnabled();
+        if (!isLocationEnabled) {
+          setState(() {
+            locationMessage = "Location services are disabled";
+          });
+          return;
+        }
+
+        // Get current position
+        Position position = await Geolocator.getCurrentPosition(
+          // ignore: deprecated_member_use
+          desiredAccuracy: LocationAccuracy.high,
+        );
+        print(position ?? "Position is null");
+
+        setState(() {
+          locationMessage =
+              "Latitude: ${position.latitude}, Longitude: ${position.longitude}";
+          // print(locationMessage);
+        });
+      } else {
+        setState(() {
+          locationMessage = "Permission denied in the processing for time";
+        });
+      }
+    } catch (e) {
+      setState(() {
+        locationMessage = "Error getting location: ${e.toString()}";
+      });
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(locationMessage ?? "Fetching location..."),
+            ElevatedButton(
+              onPressed: () {
+                print("refresh button pressed");
+                getCurrentLocation();
+              },
+              child: const Text("Refresh Location"),
+            ),
+          ],
+        ),
+      ),
+
+    );
+  }
+}
--- weather_app/lib/main.dart
+++ weather_app/lib/main.dart
@@ -1,66 +1,67 @@
 import 'package:flutter/material.dart';
 import 'package:geolocator/geolocator.dart';
 import 'package:permission_handler/permission_handler.dart';
+import "getLocation.dart";
 
 void main() {
   runApp(const MyWidget());
 }
 
-class MyWidget extends StatefulWidget {
+class MyWidget extends StatelessWidget {
   const MyWidget({super.key});
 
-  @override
-  State<MyWidget> createState() => _MyWidgetState();
-}
+//   @override
+//   State<MyWidget> createState() => _MyWidgetState();
+// }
 
-class _MyWidgetState extends State<MyWidget> {
-  String? locationMessage;
+// class _MyWidgetState extends State<MyWidget> {
+  // String? locationMessage;
 
-  // @override
-  // void initState() {
-  //   super.initState();
-  //   getCurrentLocation();
+  // // @override
+  // // void initState() {
+  // //   super.initState();
+  // //   getCurrentLocation();
+  // // }
+
+  // Future<void> getCurrentLocation() async {
+  //   print("getCurrentLocation method started");
+  //   try {
+  //     // Request location permission
+  //     PermissionStatus permissionStatus = await Permission.location.request();
+
+  //     if (permissionStatus.isGranted) {
+  //       // Check if location services are enabled
+  //       bool isLocationEnabled = await Geolocator.isLocationServiceEnabled();
+  //       if (!isLocationEnabled) {
+  //         setState(() {
+  //           locationMessage = "Location services are disabled";
+  //         });
+  //         return;
+  //       }
+
+  //       // Get current position
+  //       Position position = await Geolocator.getCurrentPosition(
+  //         // ignore: deprecated_member_use
+  //         desiredAccuracy: LocationAccuracy.high,
+  //       );
+  //       print(position ?? "Position is null");
+
+  //       setState(() {
+  //         locationMessage =
+  //             "Latitude: ${position.latitude}, Longitude: ${position.longitude}";
+  //         // print(locationMessage);
+  //       });
+  //     } else {
+  //       setState(() {
+  //         locationMessage = "Permission denied in the processing for time";
+  //       });
+  //     }
+  //   } catch (e) {
+  //     setState(() {
+  //       locationMessage = "Error getting location: ${e.toString()}";
+  //     });
+  //   }
   // }
-
-  Future<void> getCurrentLocation() async {
-    print("getCurrentLocation method started");
-    try {
-      // Request location permission
-      PermissionStatus permissionStatus = await Permission.location.request();
-
-      if (permissionStatus.isGranted) {
-        // Check if location services are enabled
-        bool isLocationEnabled = await Geolocator.isLocationServiceEnabled();
-        if (!isLocationEnabled) {
-          setState(() {
-            locationMessage = "Location services are disabled";
-          });
-          return;
-        }
-
-        // Get current position
-        Position position = await Geolocator.getCurrentPosition(
-          // ignore: deprecated_member_use
-          desiredAccuracy: LocationAccuracy.high,
-        );
-        print(position ?? "Position is null");
-
-        setState(() {
-          locationMessage =
-              "Latitude: ${position.latitude}, Longitude: ${position.longitude}";
-          // print(locationMessage);
-        });
-      } else {
-        setState(() {
-          locationMessage = "Permission denied in the processing for time";
-        });
-      }
-    } catch (e) {
-      setState(() {
-        locationMessage = "Error getting location: ${e.toString()}";
-      });
-    }
-  }
 
   @override
   Widget build(BuildContext context) {
@@ -72,21 +73,7 @@ class _MyWidgetState extends State<MyWidget> {
           title: const Text('Flutter Demo Home Page'),
           backgroundColor: Colors.amber,
         ),
-        body: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Text(locationMessage ?? "Fetching location..."),
-              ElevatedButton(
-                onPressed: (){
-                  print("refresh button pressed");
-                  getCurrentLocation();
-                },
-                child: const Text("Refresh Location"),
-              ),
-            ],
-          ),
-        ),
+        body: MyLocation(),
       ),
     );
   }
